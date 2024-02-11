8d781abc392181454662afc12ba405e2ed5e3752
anwar-2000 <khaledkhaouani5@gmail.com>
2024-02-11 09:12:38 +0100

    Bug : selection first row | NEXT : refactor table logic to useReducer

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,39 +1,105 @@
-import {useContext } from 'react';
-import FileContext from "../store/FileContext.js"
-import classes from "../styles/table.module.css"
+import { useRef } from "react";
+import { useContext, useState } from "react";
+import FileContext from "../store/FileContext.js";
+import classes from "../styles/table.module.css";
 const Table = () => {
-    const {file} = useContext(FileContext)
-
-    const handleDoubleClickTitle = (index) => {
-        console.log(index,file.headers[index])
+  const { file } = useContext(FileContext);
+  const [selectedText,setSelectedText] = useState({
+    indexRow : null,
+    indexItem : null,
+    content : "",
+  })
+  const ItemContent = useRef(null)
+  const handleDoubleClickTitle = (index) => {
+    //console.log(index, file.headers[index]);
+    setSelectedText({indexRow : null , indexItem : index , content : file.headers[index]})
+  };
+  const handleDoubleClickRow = (index) => {
+   // console.log(index, file.rows[index]);
+  };
+  const handleDoubleClickItem = (indexRow, indexItem) => {
+   // console.log(indexRow, indexItem, file.rows[indexRow]);
+    //when indexItem is null
+    if(!indexItem){
+      setSelectedText({indexRow : indexRow , indexItem : null , content : file.rows[indexRow]})
+      return;
     }
-    const handleDoubleClickRow = (index) => {
-        console.log(index,file.rows[index])
+    //console.log(indexRow, indexItem, file.rows[indexRow][indexItem]);
+    setSelectedText({indexRow : indexRow , indexItem : indexItem , content : file.rows[indexRow][indexItem] })
+  };
+  const handleTitleItemTextChange = (state) => {
+    const newValue = ItemContent.current.value;
+    if(selectedText.content !== newValue){
+     file.headers[selectedText.indexItem] = newValue
     }
-    const handleDoubleClickItem = (indexRow,indexItem) => {
-        console.log(indexRow,indexItem,file.rows[indexRow][indexItem])
+    setSelectedText({indexItem : null , indexRow : null , content : ""})
+  }
+  const handleRowItemTextChange = (state) => {
+    const newValue = ItemContent.current.value;
+    if(selectedText.content !== newValue){
+      // duplex means 2 , we have two pointers of row item , otherwise its just 1
+     state === "duplex" ? file.rows[selectedText.indexRow][selectedText.indexItem] = newValue : file.rows[selectedText.indexRow] = newValue
+    }else{
+      return;
     }
-    return <table className={classes.table}>
-    <thead>
-      <tr>
-        {file.headers.length > 1  ? file.headers.map((value,i)=>(
-        <th key={i}  onDoubleClick={()=>handleDoubleClickTitle(i)}>{value}</th>
-        )) :  Array(file.headers.map((_,i)=>(
-          <th key={i} onDoubleClick={()=>handleDoubleClickTitle(i)}>Header</th>
-        )))}
-      </tr>
-    </thead>
-    <tbody>
-    </tbody>
-        {file.rows.map((item,j)=>(
-           <tr key={j} onDoubleClick={()=>handleDoubleClickRow(j)}>
-            {item.length > 1 ? item.map((value,i)=>(
-            <td key={i} onDoubleClick={()=>handleDoubleClickItem(j,i)} >{value}</td>
-            )) : 
-            <td key={j} onDoubleClick={()=>handleDoubleClickItem(j,i)}>{item}</td>}
-         </tr>
-        ))}
+    setSelectedText({indexItem : null , indexRow : null , content : ""})
+  }
+  return (
+    <table className={classes.table}>
+      <thead>
+        <tr>
+          {file.headers.length > 1
+            ? file.headers.map((value, i) => (
+                <th key={i} onDoubleClick={() => handleDoubleClickTitle(i)}>
+                  {
+                ( selectedText.indexItem === i) ?
+                      <input ref={ItemContent} onBlur={handleTitleItemTextChange} type="text" name="content" defaultValue={selectedText.content} /> 
+                 :
+                  value
+                }
+                </th>
+              ))
+            : <th onDoubleClick={() => handleDoubleClickTitle(0)}>
+                    {
+                (selectedText.indexItem === 0) ?
+                      <input ref={ItemContent} onBlur={handleTitleItemTextChange} type="text" name="content" defaultValue={selectedText.content} /> 
+                 :
+                  "Change header"
+                }
+                  </th>}
+              
+              
+        </tr>
+      </thead>
+      <tbody>
+      {file.rows.map((item, j) => (
+        <tr key={j} onDoubleClick={() => handleDoubleClickRow(j)}>
+          {item.length > 1 ? (
+            item.map((value, i) => (
+              <td key={i} onDoubleClick={() => handleDoubleClickItem(j, i)}>
+                {
+                (selectedText.indexRow === j && selectedText.indexItem === i) ?
+                      <input ref={ItemContent} onBlur={()=>handleRowItemTextChange("duplex")} type="text" name="content" defaultValue={selectedText.content} /> 
+                 :
+                  value
+                }
+              </td>
+            ))
+          ) : (
+            <td key={j} onDoubleClick={() => handleDoubleClickItem(j,null)}>
+              {
+                (selectedText.indexRow === j) ?
+                      <input ref={ItemContent} onBlur={()=>handleRowItemTextChange("only one index")} type="text" name="content" defaultValue={selectedText.content} /> 
+                 :
+                  item
+                }
+            </td>
+          )}
+        </tr>
+      ))}
+      </tbody>
     </table>
-}
+  );
+};
 
 export default Table;
--- src/pages/Visualize.jsx
+++ src/pages/Visualize.jsx
@@ -3,13 +3,15 @@ import FileContext from "../store/FileContext.js"
 import Table from "../components/Table"
 import classes from "../styles/visualize.module.css"
 import { BsDownload } from "react-icons/bs";
+import { TfiSave } from "react-icons/tfi";
+
 
 function Visualize() {
   const {file} = useContext(FileContext)
   return <>
-  <div className="">
+  <div className={classes.VisualizeHeader}>
     <h4 className={classes.title}>File : {file.name}  <BsDownload size={20} style={{cursor : "pointer"}}/> </h4>
-   
+    <TfiSave size={20} color="black" style={{cursor : "pointer"}} />
   </div>
     <div className={classes.visualize_table}>
        <Table />
